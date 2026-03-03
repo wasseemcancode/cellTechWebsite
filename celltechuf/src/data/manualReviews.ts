582281786c6ee89c64f8d5252d537d7a4ccdfa89
Wasseem Barghouthi <wasseembarghouthi@Wasseems-MacBook-Pro.local>
2026-03-03 10:24:53 -0500

    Add manual review: John Cardoza

--- celltechuf/src/data/manualReviews.ts
+++ celltechuf/src/data/manualReviews.ts
@@ -20,5 +20,5 @@ export const manualReviews: ManualReviewEntry[] = [
   // { name: 'Sarah M.', text: 'Super fast screen repair!', date: '2024-11-15' },
   // { name: 'Jake K.', text: 'Fixed my charging port same day.', date: '2024-10-03' },
   {name: 'Naser Oqab', text: 'Wasseem is amazing! Super professional, fast, and knows exactly what he’s doing. He got the job done quickly and made the whole process smooth and easy. Highly reccomend to any college student looking for reliability and speed.', date: '2026-02-25'},
-  
+  {name: 'John Cardoza', text: 'Needed to get my PS5 fixed and Wasseem fixed it quick, I got it back the same day and he was super nice. Will surely be going there when I need anything fixed. Thanks Wasseem 👍', date: '2026-02-19'}
 ]
